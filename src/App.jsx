97fcc7bd3fc4d690bde5168bc0cd49c3319ccef0
sd025 <shubham.dhumal452@gmail.com>
2024-06-13 11:01:52 +0530

    table search and paginate

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import Example from "./components/Table";
-import Search from "./screen/Search";
 
 export default function App() {
   return (
     <div>
-      {/* <Search /> */}
       <Example />
     </div>
   )
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,55 +1,55 @@
-import { useMemo } from 'react';
+import { useMemo } from "react";
 import {
   MaterialReactTable,
   useMaterialReactTable,
-} from 'material-react-table';
+} from "material-react-table";
 
 //nested data is ok, see accessorKeys in ColumnDef below
 const data = [
   {
     name: {
-      firstName: 'John',
-      lastName: 'Doe',
+      firstName: "John",
+      lastName: "Doe",
     },
-    address: '261 Erdman Ford',
-    city: 'East Daphne',
-    state: 'Kentucky',
+    address: "261 Erdman Ford",
+    city: "East Daphne",
+    state: "Kentucky",
   },
   {
     name: {
-      firstName: 'Jane',
-      lastName: 'Doe',
+      firstName: "Jane",
+      lastName: "Doe",
     },
-    address: '769 Dominic Grove',
-    city: 'Columbus',
-    state: 'Ohio',
+    address: "769 Dominic Grove",
+    city: "Columbus",
+    state: "Ohio",
   },
   {
     name: {
-      firstName: 'Joe',
-      lastName: 'Doe',
+      firstName: "Joe",
+      lastName: "Doe",
     },
-    address: '566 Brakus Inlet',
-    city: 'South Linda',
-    state: 'West Virginia',
+    address: "566 Brakus Inlet",
+    city: "South Linda",
+    state: "West Virginia",
   },
   {
     name: {
-      firstName: 'Kevin',
-      lastName: 'Vandy',
+      firstName: "Kevin",
+      lastName: "Vandy",
     },
-    address: '722 Emie Stream',
-    city: 'Lincoln',
-    state: 'Nebraska',
+    address: "722 Emie Stream",
+    city: "Lincoln",
+    state: "Nebraska",
   },
   {
     name: {
-      firstName: 'Joshua',
-      lastName: 'Rolluffs',
+      firstName: "Joshua",
+      lastName: "Rolluffs",
     },
-    address: '32188 Larkin Turnpike',
-    city: 'Charleston',
-    state: 'South Carolina',
+    address: "32188 Larkin Turnpike",
+    city: "Charleston",
+    state: "South Carolina",
   },
 ];
 
@@ -58,37 +58,60 @@ const Example = () => {
   const columns = useMemo(
     () => [
       {
-        accessorKey: 'name.firstName', //access nested data with dot notation
-        header: 'First Name',
+        accessorKey: "name.firstName", //access nested data with dot notation
+        header: "First Name",
         size: 150,
       },
       {
-        accessorKey: 'name.lastName',
-        header: 'Last Name',
+        accessorKey: "name.lastName",
+        header: "Last Name",
         size: 150,
       },
       {
-        accessorKey: 'address', //normal accessorKey
-        header: 'Address',
+        accessorKey: "address", //normal accessorKey
+        header: "Address",
         size: 200,
       },
       {
-        accessorKey: 'city',
-        header: 'City',
+        accessorKey: "city",
+        header: "City",
         size: 150,
       },
       {
-        accessorKey: 'state',
-        header: 'State',
+        accessorKey: "state",
+        header: "State",
         size: 150,
       },
     ],
-    [],
+    []
   );
 
   const table = useMaterialReactTable({
     columns,
-    data, //data must be memoized or stable (useState, useMemo, defined outside of this component, etc.)
+    data,
+    enableColumnFilterModes: true,
+    enableColumnOrdering: true,
+    enableGrouping: false,
+    enableColumnPinning: false,
+    enableFacetedValues: true,
+    enableRowActions: false,
+    enableRowSelection: false,
+    initialState: {
+      showColumnFilters: false,
+      showGlobalFilter: true,
+    },
+    enablePagination: true,
+    muiPaginationProps: {
+      color: 'standard',
+      shape: "rounded",
+      showRowsPerPage: false,
+      variant: "outlined",
+    },
+    paginationDisplayMode: "pages",
+    muiSearchTextFieldProps: {
+      size: "small",
+      variant: "outlined",
+    },
   });
 
   return <MaterialReactTable table={table} />;
